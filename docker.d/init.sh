37d889cf66d8c35dd98bda973b555459a52ed022
Rok Garbas <rok@garbas.si>
2019-07-25 14:36:35 +0200

    another rework and cleanup

--- docker.d/init.sh
+++ docker.d/init.sh
@@ -1,30 +1,58 @@
 #!/bin/bash
 set -e
 
-# == START: This system variables are set by cloudops ==
+export ARTIFACTS_DIR=/app/artifacts
+export CONFIG_DIR=/app/configs
+export CONFIG_LOADER=/app/bin/configloader
+export ED25519_PRIVKEY_PATH=$CONFIG_DIR/ed25519_privkey
+export LOGS_DIR=/app/logs
+export SCRIPTWORKER=/app/bin/scriptworker
+export TASK_CONFIG=$CONFIG_DIR/worker.json
+export TASK_LOGS_DIR=$ARTIFACTS_DIR/public/logs
+export TEMPLATE_DIR=/app/docker.d
+export WORK_DIR=/app/workdir
+
+# == START: this is what we need to configure ==
 test $PROJECT_NAME
-test $ENV  # ENV should be set to "dev", "stage" or "prod"
-test $COT_PRODUCT  # either "firefox" or "thunderbird"
+test $ENV
+test $COT_PRODUCT
 test $TASKCLUSTER_CLIENT_ID
 test $TASKCLUSTER_ACCESS_TOKEN
-#test $ED25519_PRIVKEY  # optional since on staging we don't sign CoT files
-# == END ==
+if [ "$ENV" == "prod" ]; then
+  test $ED25519_PRIVKEY
+fi
+# == END:   this is what we need to configure ==
 
-TEMPLATEDIR=/app/docker.d/configs
-CONFIGDIR=/app/configs
-CONFIGLOADER=/app/bin/configloader
-SCRIPTWORKER=/app/bin/scriptworker
+export PROVISIONER_ID=scriptworker-k8s-v1
+export WORKER_GROUP="${PROJECT_NAME}script-${COT_PRODUCT}-${ENV}-v1"
+export WORKER_TYPE="${PROJECT_NAME}script-${COT_PRODUCT}-${ENV}-v1"
+export WORKER_ID_PREFIX="${PROJECT_NAME}script-${COT_PRODUCT}-${ENV}-"
+export TASK_SCRIPT=/app/bin/${PROJECT_NAME}script
+export VERBOSE=true
+export ARTIFACT_UPLOAD_TIMEOUT=1200
+export GITHUB_OAUTH_TOKEN=
+export TASK_MAX_TIMEOUT=3600
+export VERIFY_CHAIN_OF_TRUST=true
+export SIGN_CHAIN_OF_TRUST=false
+if [ "$ENV" == "prod" ]; then
+  export SIGN_CHAIN_OF_TRUST=true
+fi
+export VERIFY_COT_SIGNATURE=false
+if [ "$ENV" == "prod" ]; then
+  export VERIFY_COT_SIGNATURE=true
+fi
 
-# export JSON-e related vars
-export TASK_SCRIPT_CONFIG="$CONFIGDIR/worker.json"
+mkdir -p -m 700 $CONFIG_DIR
 
-mkdir -p -m 700 $CONFIGDIR
+export INIT_WORKER=$(dirname $0)/init_worker.sh
+if [ -e $INIT_WORKER ]; then
+  source $INIT_WORKER
+fi
 
-# Eval JSON-e expressions in the config templates
-$CONFIGLOADER --worker-id-prefix=${PROJECT_NAME}script-${ENV}- $TEMPLATEDIR/scriptworker.yaml $CONFIGDIR/scriptworker.json
-$CONFIGLOADER $TEMPLATEDIR/worker.json $CONFIGDIR/worker_config.json
+$CONFIG_LOADER --worker-id-prefix=$WORKER_ID_PREFIX $TEMPLATE_DIR/scriptworker.yml $CONFIG_DIR/scriptworker.json
+$CONFIG_LOADER $TEMPLATE_DIR/worker.yml $CONFIG_DIR/worker.json
 
-echo $ED25519_PRIVKEY > $CONFIGDIR/ed25519_privkey
-chmod 600 $CONFIGDIR/ed25519_privkey
+echo $ED25519_PRIVKEY > $ED25519_PRIVKEY_PATH
+chmod 600 $ED25519_PRIVKEY_PATH
 
-exec $SCRIPTWORKER $CONFIGDIR/scriptworker.json
+exec $SCRIPTWORKER $CONFIG_DIR/scriptworker.json
